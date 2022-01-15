2266e99515cb9b6b7612043dd001d847b4c2dfa5
Sqzjelele <jakub.kowalczyk1280@gmail.com>
2022-01-15 20:01:12 +0100

    Add project files.

---
+++ Lab4/Data/Lab4Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lab4.Models;
+
+namespace Lab4.Data
+{
+    public class Lab4Context : DbContext
+    {
+        public Lab4Context(DbContextOptions<Lab4Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab4.Models.Car> Car { get; set; }
+    }
+}
---
+++ Lab4/Models/SeedData.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Lab4.Data;
+using System;
+using System.Linq;
+namespace Lab4.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new Lab4Context(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<Lab4Context>>()))
+            {
+                // Look for any cars.
+                if (context.Car.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Car.AddRange(
+                    new Car
+                    {
+                        Brand = "Audi",
+                        Model = "e-tron GT quattro",
+                        YearOfManufacture = DateTime.Parse("2022-1-1"),
+                        FuelType = "Petrol",
+                        Price = 100000M
+                    },
+                    new Car
+                    {
+                        Brand = "Audi",
+                        Model = "A1 Sportback",
+                        YearOfManufacture = DateTime.Parse("2019-1-1"),
+                        FuelType = "Diesel",
+                        Price = 25000M
+                    },
+                    new Car
+                    {
+                        Brand = "Audi",
+                        Model = "S3",
+                        YearOfManufacture = DateTime.Parse("2019-1-1"),
+                        FuelType = "Petrol",
+                        Price = 50000M
+                    },
+                    new Car
+                    {
+                        Brand = "Audi",
+                        Model = "A6 Avant",
+                        YearOfManufacture = DateTime.Parse("2020-1-1"),
+                        FuelType = "Diesel",
+                        Price = 70000M
+                    },
+                    new Car
+                    {
+                        Brand = "Citroen",
+                        Model = "Berlingo",
+                        YearOfManufacture = DateTime.Parse("2022-1-1"),
+                        FuelType = "Petrol",
+                        Price = 30000M
+                    },
+                    new Car
+                    {
+                        Brand = "Citroen",
+                        Model = "C5 Aircross",
+                        YearOfManufacture = DateTime.Parse("2018-1-1"),
+                        FuelType = "Petrol",
+                        Price = 27000M
+                    },
+                    new Car
+                    {
+                        Brand = "Citroen",
+                        Model = "C3",
+                        YearOfManufacture = DateTime.Parse("2017-1-1"),
+                        FuelType = "Petrol",
+                        Price = 12000M
+                    },
+                    new Car
+                    {
+                        Brand = "Citroen",
+                        Model = "DS4",
+                        YearOfManufacture = DateTime.Parse("2012-1-1"),
+                        FuelType = "Petrol",
+                        Price = 10000M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
